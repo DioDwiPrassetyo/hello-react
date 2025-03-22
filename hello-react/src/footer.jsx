b9cd82729c60b3642a394f4004b6969cdfe9907b
DioDwiPrassetyo <diodwiprassetyo01@gmail.com>
2025-03-22 20:28:29 +0700

    Add files via upload

---
+++ hello-react/src/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <div>
+            <h3>Ini adalah halaman Footer</h3>
+            <footer>🚀 Design by Nana🤫</footer>
+        </div>
+    );
+}
+export default Footer;
